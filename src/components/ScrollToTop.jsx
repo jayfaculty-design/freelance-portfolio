2a5415878aa51076819cd80014c3b19c163b1cc4
Jay Designs <izfaculty12@gmail.com>
2025-03-01 12:43:44 +0000

    motion fixed

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "motion/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { debounce } from "../utils/debounce";
